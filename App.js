062e08631c08c611e5a15999847f09688107c987
sbuckles10 <sbuckles88@gmail.com>
2021-11-09 11:24:35 -0800

    update

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import title from './title';
 
 function App() {
   return (
@@ -7,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
